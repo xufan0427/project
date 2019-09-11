c14649f5c749d446afe80befce1a56ec32a6a997
徐凡 <569025334@qq.com>
2019-09-11 15:10:22 +0800

    修改编码

--- sprincloud-root/springcloud-eureka/src/main/java/com/controller/ManagerComtroller.java
+++ sprincloud-root/springcloud-eureka/src/main/java/com/controller/ManagerComtroller.java
@@ -6,21 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fan.mapper.TbUserMapper;
+
 import com.fan.pojo.TbUser;
 import com.fan.service.UserService;
 
 @CrossOrigin
 @RestController	
 @RequestMapping("/manager")
-public class ManagerComtroller {
+public class ManagerComtroller {   
 
 	@Autowired
 	private UserService userService;
 	
 	@RequestMapping("/getUser")
 	public String getUserInfo(@RequestParam(required=true) Long userId) {
-		System.out.println("用户名---"+userId+"--------");
+		System.out.println("用户id---"+userId+"--------");
 		TbUser TbUser = userService.getUser(userId);
 		return TbUser.getUsername();
 	}
